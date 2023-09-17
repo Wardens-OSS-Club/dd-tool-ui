66b460aba81e6269e54ac9197a3fa297ac86de6b
Entreprenerd <alex@entreprenerd.xyz>
2023-09-17 18:09:19 +0200

    chore: initial setup

---
+++ mock/task.ts
@@ -0,0 +1,18 @@
+const MOCK_TASK = {
+  "call": {
+    "callInfo": {
+      "value": null,
+      "gasLimit": null,
+      "from": "0xBA485b556399123261a5F9c95d413B4f93107407"
+    },
+    "contract": {
+      "functionString": "function getPricePerFullShare() external view returns (uint256)",
+      "address": "0xBA485b556399123261a5F9c95d413B4f93107407",
+      "inputs": []
+    }
+  },
+  "inputMappings": [],
+  "outputMappings": ["balance"]
+}
+    
+export default MOCK_TASK
---
+++ modules.d.ts
@@ -0,0 +1,1 @@
+declare module "dd-tool-package"
