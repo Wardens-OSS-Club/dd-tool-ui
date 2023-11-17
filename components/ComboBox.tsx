24c0d6dd3e01cea2c897747f0761e190dbc80268
lokithe5th <lourenslinde@gmail.com>
2023-11-17 10:00:49 +0200

    Fix: Combobox value vs display

--- components/ComboBox.tsx
+++ components/ComboBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface ComboBoxProps {
     options: string[];
@@ -8,21 +8,28 @@ interface ComboBoxProps {
 }
 
 const ComboBox: React.FC<ComboBoxProps> = ({ options, onChange, onBlur, value }) => {
-  const [inputValue, setInputValue] = useState(value);
+  // Use state to manage the displayed value in the input
+  const [displayValue, setDisplayValue] = useState(value.replace(/^\$/, ''));
+
+  useEffect(() => {
+    // Update displayValue when the value prop changes
+    setDisplayValue(value.replace(/^\$/, ''));
+  }, [value]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-    onChange(event.target.value);
+    const newValue = event.target.value;
+    setDisplayValue(newValue);
+    onChange(`$${newValue}`); // Add the $ symbol when passing the value to the onChange prop
   };
 
   return (
     <div style={{ position: 'relative' }}>
       <input 
         type="text" 
-        value={inputValue}
+        value={displayValue} // Use displayValue for the input
         onChange={handleChange}
         onBlur={onBlur}
-        list={options && options.length > 0 ? "combo-options" : undefined} 
+        list={options && options.length > 0 ? "combo-options" : undefined}
         style={{ flex: 2, padding: '4px', borderRadius: '4px', border: '1px solid #ccc' }}
       />
       {options && options.length > 0 && (
